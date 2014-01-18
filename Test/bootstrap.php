00b3076177107cdee659cd612483e5c46089b711
Rachman Chavik <rchavik@xintesa.com>
2014-01-18 10:40:02 +0700

    Rename `Test` to `tests`

--- Test/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Test runner bootstrap.
- *
- * Add additional configuration/setup your application needs when running 
- * unit tests in this file.
- */
-require dirname(__DIR__) . '/App/Config/bootstrap.php';
---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Test runner bootstrap.
+ *
+ * Add additional configuration/setup your application needs when running
+ * unit tests in this file.
+ */
+require dirname(__DIR__) . '/src/Config/bootstrap.php';
