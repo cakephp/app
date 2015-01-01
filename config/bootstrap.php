17d7945682c7341263c27cb9e89fcb302ca27e6c
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2015-01-01 13:20:34 +0100

    Loading the Migrations plugin

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -174,6 +174,8 @@ Request::addDetector('tablet', function ($request) {
  *
  */
 
+Plugin::load('Migrations');
+
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
