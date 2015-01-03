4c76d6ecf050542e6e99de854892c59f0e9ce8a4
José Lorenzo Rodríguez <jose.zap@gmail.com>
2015-01-04 00:21:28 +0100

    Fixed grammar

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -33,7 +33,7 @@ require ROOT . DS . 'vendor' . DS . 'autoload.php';
  */
 require CORE_PATH . 'config' . DS . 'bootstrap.php';
 
-// You can remove this if you have confident you have intl installed.
+// You can remove this if you are confident you have intl installed.
 if (!extension_loaded('intl')) {
     trigger_error('You must enable the intl extension to use CakePHP.', E_USER_ERROR);
 }
