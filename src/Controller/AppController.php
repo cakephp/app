46634ffd93a3ff2f5bf5d5c2131c8cf3bdd2784e
ADmad <admad.coder@gmail.com>
2015-05-08 17:33:37 +0530

    Load RequestHandler component

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -38,6 +38,8 @@ class AppController extends Controller
     public function initialize()
     {
         parent::initialize();
+
+        $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
     }
 
