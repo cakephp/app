f16ef1ab087bb30ccfb94ad7f4f2f6574ad3b56c
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2016-01-17 18:40:05 -0500

    Use camel-case for defaultLocale

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -97,7 +97,7 @@ mb_internal_encoding(Configure::read('App.encoding'));
  * Set the default locale. This controls how dates, number and currency is
  * formatted and sets the default language to use for translations.
  */
-ini_set('intl.default_locale', Configure::read('App.default_locale'));
+ini_set('intl.default_locale', Configure::read('App.defaultLocale'));
 
 /**
  * Register application error and exception handlers.
