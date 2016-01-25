371ac89779f39f8a9932e0fd46a97edcf21f5893
saeideng <saeideng@users.noreply.github.com>
2016-01-25 17:10:26 +0330

    rename "Cake" to "App"
    ref: https://github.com/cakephp/app/pull/295

--- tests/TestCase/Controller/PagesControllerTest.php
+++ tests/TestCase/Controller/PagesControllerTest.php
@@ -12,7 +12,7 @@
  * @since         1.2.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-namespace Cake\Test\TestCase\Controller;
+namespace App\Test\TestCase\Controller;
 
 use App\Controller\PagesController;
 use Cake\Core\App;
