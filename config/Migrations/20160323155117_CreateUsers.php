e99d7577a020646eb4a7f8ce4b8a74c4d2fb33f9
Chris Hickingbottom <chickingbottom@edgecommunications.com>
2016-03-23 10:54:58 -0500

    added migration files

---
+++ config/Migrations/20160323155117_CreateUsers.php
@@ -0,0 +1,61 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('role', 'string', [
+            'default' => 'user',
+            'limit' => 50,
+            'null' => false,
+        ]);
+        $table->addColumn('email', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('first_name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('last_name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('password', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('active', 'boolean', [
+            'default' => 0,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addIndex([
+            'email',
+        ], [
+            'name' => 'EMAIL_INDEX',
+            'unique' => true,
+        ]);
+        $table->create();
+    }
+}
