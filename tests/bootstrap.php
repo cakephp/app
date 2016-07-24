5d47263a856cf32802122cdfd24da87f40972ea3
ADmad <admad.coder@gmail.com>
2016-07-25 01:01:26 +0530

    Include composer's autoloader in tests/boostrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,6 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
+require dirname(__DIR__) . '/vendor/autoload.php';
+
 require dirname(__DIR__) . '/config/bootstrap.php';
