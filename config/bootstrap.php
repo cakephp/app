817481586442a4c9b6639a95ad3857f945447e83
HT Studios <github@ht-studios.de>
2016-08-03 15:05:16 +0200

    Charge order of Plugins loaded
    This will look nicer when plugins are being added via `cake bake plugin` or `composer require`.
    Followup to https://github.com/cakephp/app/pull/407

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -204,10 +204,10 @@ Type::build('datetime')
  *
  */
 
-Plugin::load('Migrations');
-
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
+
+Plugin::load('Migrations');
