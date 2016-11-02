fbe3753867dbbbd72bcb6d9e7f017ce525368e2f
Jorge M. González Martín <steinkel@gmail.com>
2016-11-02 19:35:32 +0000

    fix cs issues

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -44,9 +44,9 @@ class AppController extends Controller
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
 
-        /* 
-         * Enable the following components for recommended CakePHP security settings. 
-         * see http://book.cakephp.org/3.0/en/controllers/components/security.html 
+        /*
+         * Enable the following components for recommended CakePHP security settings.
+         * see http://book.cakephp.org/3.0/en/controllers/components/security.html
          */
         //$this->loadComponent('Security');
         //$this->loadComponent('Csrf');
