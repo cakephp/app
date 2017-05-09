e7a32d8848e0b45b62bf4e36579f3f18c6a9155b
Mark Sch <dereuromark@users.noreply.github.com>
2017-05-09 21:51:24 +0200

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -31,7 +31,7 @@ class Application extends BaseApplication
     /**
      * Setup the middleware queue your application will use.
      *
-     * @param \Cake\Http\MiddlewareQueue $middlewareStack The middleware queue to setup.
+     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
      * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
      */
     public function middleware($middlewareQueue)
