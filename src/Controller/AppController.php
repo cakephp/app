811b09266246eaf0e507eebad0c042d131dd5548
ADmad <ADmad@users.noreply.github.com>
2017-06-22 23:02:31 +0530

    Add missing "not".

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -61,7 +61,7 @@ class AppController extends Controller
     public function beforeRender(Event $event)
     {
         // Note: These defaults are just to get started quickly with development
-        // and should be used in production. You should instead set "_serialize"
+        // and should not be used in production. You should instead set "_serialize"
         // in each action as required.
         if (!array_key_exists('_serialize', $this->viewVars) &&
             in_array($this->response->type(), ['application/json', 'application/xml'])
