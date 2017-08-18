a231d485063b09c356601ced116dfd31c1616291
inoas <mail@inoas.com>
2017-08-18 17:14:31 +0200

    enable queries logging by default

--- config/app.default.php
+++ config/app.default.php
@@ -236,7 +236,7 @@ return [
             'timezone' => 'UTC',
             'flags' => [],
             'cacheMetadata' => true,
-            'log' => false,
+            'log' => true,
 
             /**
              * Set identifier quoting to true if you are using reserved words or
