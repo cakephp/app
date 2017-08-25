2be57b6450196335ef1a20ecd6a193bfae2f6a41
ADmad <admad.coder@gmail.com>
2017-08-26 01:55:37 +0530

    Making usage of `.env` file opt-in instead of opt-out.
    Also comment out important vars in `.env.default` so that after copying it to `.env`
    users have to explicitly uncomment and set proper values.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -45,18 +45,17 @@ use Cake\Utility\Inflector;
 use Cake\Utility\Security;
 
 /**
- * Read .env file if APP_NAME is not set.
- *
- * You can remove this block if you do not want to use environment
- * variables for configuration when deploying.
+ * Uncomment block of code below if you want to `.env` file during development.
+ * You should copy `config/.env.default to `config/.env` and set/modify the
+ * variables as required.
  */
-if (!env('APP_NAME') && file_exists(CONFIG . '.env')) {
-    $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . '.env']);
-    $dotenv->parse()
-        ->putenv()
-        ->toEnv()
-        ->toServer();
-}
+// if (!env('APP_NAME') && file_exists(CONFIG . '.env')) {
+//     $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . '.env']);
+//     $dotenv->parse()
+//         ->putenv()
+//         ->toEnv()
+//         ->toServer();
+// }
 
 /*
  * Read configuration file and inject configuration into various
