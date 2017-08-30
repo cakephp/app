cbf48f3e5170aede9ccf6a9fcb212c79b9a87c4a
Marcelo Rocha <mrcodex@msn.com>
2017-08-30 11:44:19 -0300

    Using App\Application::bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,10 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
+use App\Application;
+
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-require dirname(__DIR__) . '/config/bootstrap.php';
-
+$app = new Application(dirname(__DIR__) . '/config');
+$app->bootstrap();
 $_SERVER['PHP_SELF'] = '/';
