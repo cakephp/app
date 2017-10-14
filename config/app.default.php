9bcd7e7ccc011da6ad63aacddb9b17cf9e59911e
inoas <mail@inoas.com>
2017-10-15 00:50:16 +0200

    Use UTF-8 Full Unicode / 4 Byte by default
    This change affects MySQL and MariaDB.
    Otherwise utf8 support will mean your data gets truncated once a 4-byte utf8 symbol gets inserted such as `'😃'`

--- config/app.default.php
+++ config/app.default.php
@@ -232,7 +232,8 @@ return [
             'username' => 'my_app',
             'password' => 'secret',
             'database' => 'my_app',
-            'encoding' => 'utf8',
+            // Non MariaDB/MySQL equivalent is 'utf8'
+            'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
             'flags' => [],
             'cacheMetadata' => true,
@@ -272,7 +273,8 @@ return [
             'username' => 'my_app',
             'password' => 'secret',
             'database' => 'test_myapp',
-            'encoding' => 'utf8',
+            // Non MariaDB/MySQL equivalent is 'utf8'
+            'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
             'cacheMetadata' => true,
             'quoteIdentifiers' => false,
