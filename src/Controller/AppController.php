291341943dce0a9e8ba9fd353b6755f6c4a1bc0d
Samuel FORESTIER <sam.forestier@gmail.com>
2017-10-17 13:24:19 -0400

    Simplifies the concept with `strpos()` and `Response::getHeaderLine()`

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -55,24 +55,19 @@ class AppController extends Controller
     /**
      * Before render callback.
      *
-     * @param \Cake\Event\Event $event The beforeRender event.
+     * @param  \Cake\Event\Event $event The beforeRender event.
      * @return \Cake\Http\Response|null|void
      */
     public function beforeRender(Event $event)
     {
         // Note: These defaults are just to get started quickly with development
-        // and should not be used in production. You should instead set "_serialize"
-        // in each action as required.
+        // and should not be used in production.
+        // You should instead set "_serialize" in each action as required.
         if (!array_key_exists('_serialize', $this->viewVars)) {
-            $mimeType = $this->RequestHandler->mapAlias(
-                $this->RequestHandler->responseType()
-            );
-            if (!is_array($mimeType)) {
-                if (in_array($mimeType, ['application/json', 'application/xml'])) {
-                    $this->set('_serialize', true);
-                }
-            } elseif (in_array('application/json', $mimeType) ||
-                      in_array('application/xml', $mimeType)) {
+            $contentType = $this->response->getHeaderLine('Content-Type');
+            if (strpos($contentType, 'application/json') !== false ||
+                strpos($contentType, 'application/xml') !== false
+            ) {
                 $this->set('_serialize', true);
             }
         }
