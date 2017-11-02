b06c605591d299b1769b5a276181cc2da15c8c1e
Samuel FORESTIER <HorlogeSkynet@users.noreply.github.com>
2017-11-02 10:08:22 -0400

    Replaces deprecated `Response::type()` by `Response::getType()` (#555) (#557)
    * Replaces deprecated `Response::type()` by new `Response::getType()`

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -61,10 +61,10 @@ class AppController extends Controller
     public function beforeRender(Event $event)
     {
         // Note: These defaults are just to get started quickly with development
-        // and should not be used in production. You should instead set "_serialize"
-        // in each action as required.
+        // and should not be used in production.
+        // You should instead set "_serialize" in each action as required.
         if (!array_key_exists('_serialize', $this->viewVars) &&
-            in_array($this->response->type(), ['application/json', 'application/xml'])
+            in_array($this->response->getType(), ['application/json', 'application/xml'])
         ) {
             $this->set('_serialize', true);
         }
