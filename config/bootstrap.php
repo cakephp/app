6e7991d986fa861f6c9ccf5d22325abd60a548b9
Kazuki_Kamizuru <k.amago0721@gmail.com>
2018-02-19 21:37:16 +0900

    Set timezone from .env

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -89,11 +89,11 @@ if (Configure::read('debug')) {
 }
 
 /*
- * Set server timezone to UTC. You can change it to another timezone of your
+ * Set the default server timezone. You can change it to another timezone of your
  * choice but using UTC makes time calculations / conversions easier.
  * Check http://php.net/manual/en/timezones.php for list of valid timezone strings.
  */
-date_default_timezone_set('UTC');
+date_default_timezone_set(Configure::read('App.defaultTimezone'));
 
 /*
  * Configure the mbstring extension to use the correct encoding.
