f899cab0848f9da531d453962ea2cc50cd20b112
saeid <saeideng@yahoo.com>
2018-06-28 01:13:52 +0430

    no needed to `['bootstrap' => true]` config for DebugKit

--- src/Application.php
+++ src/Application.php
@@ -51,7 +51,7 @@ class Application extends BaseApplication
          * Debug Kit should not be installed on a production system
          */
         if (Configure::read('debug')) {
-            $this->addPlugin(\DebugKit\Plugin::class, ['bootstrap' => true]);
+            $this->addPlugin(\DebugKit\Plugin::class);
         }
     }
 
