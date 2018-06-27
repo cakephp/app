c6d8a606190e8589297227e1db3946d74d5a786c
saeid <saeideng@yahoo.com>
2018-06-28 00:42:32 +0430

    move loading plugin into Application

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -207,11 +207,3 @@ Type::build('timestamp')
  * Plugin::load('Migrations'); //Loads a single plugin named Migrations
  *
  */
-
-/*
- * Only try to load DebugKit in development mode
- * Debug Kit should not be installed on a production system
- */
-if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
-}
