b9e71ce1cbf09f270f54515ccc47a8f260c11113
Flavius <highstrike@gmail.com>
2018-07-10 17:47:57 +0300

    Update ErrorController.php
    completes #592

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -30,7 +30,9 @@ class ErrorController extends AppController
      */
     public function initialize()
     {
-        $this->loadComponent('RequestHandler');
+        $this->loadComponent('RequestHandler', [
+            'enableBeforeRedirect' => false,
+        ]);
     }
 
     /**
