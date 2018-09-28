f3e77a07524415429014005f8952800fb83bbb7a
José Lorenzo Rodríguez <lorenzo@users.noreply.github.com>
2018-09-28 15:32:07 +0200

    Some more help on when to use .env files

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -48,6 +48,10 @@ use Cake\Utility\Security;
  * Uncomment block of code below if you want to use `.env` file during development.
  * You should copy `config/.env.default to `config/.env` and set/modify the
  * variables as required.
+ *
+ * It is HIGHLY discouraged to use a .env file in production, due to security risks
+ * and decresed performance on each request. The purpose of the .env file is to emulate
+ * the presence of the environment variables like they would be present in production.
  */
 // if (!env('APP_NAME') && file_exists(CONFIG . '.env')) {
 //     $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . '.env']);
