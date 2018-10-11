040cddab8e13960e5ebce5dcc3dadbedde39e683
Mark Story <mark@mark-story.com>
2018-10-10 22:31:52 -0400

    Move middleware declaration to routes.php
    While defining middleware in Application::routes() may be more correct
    to some, it is incompatible with route caching which is something that
    is enabled by default in the application skeleton.

--- config/routes.php
+++ config/routes.php
@@ -17,8 +17,8 @@
  * @link          https://cakephp.org CakePHP(tm) Project
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
-
 use Cake\Core\Plugin;
+use Cake\Http\Middleware\CsrfProtectionMiddleware;
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
@@ -47,6 +47,12 @@ use Cake\Routing\Route\DashedRoute;
 Router::defaultRouteClass(DashedRoute::class);
 
 Router::scope('/', function (RouteBuilder $routes) {
+        // Register scoped middleware for use in routes.php
+        $routes->registerMiddleware('csrf', new CsrfProtectionMiddleware([
+            'httpOnly' => true
+        ]));
+
+
     /**
      * Apply a middleware to the current route scope.
      * Requires middleware to be registered via `Application::routes()` with `registerMiddleware()`
