7f2767986dc9cd72beca5426af03d2c64aec89f8
ADmad <admad.coder@gmail.com>
2018-11-19 17:55:38 +0530

    Update default path to locales folder.
    Refs cakephp/cakephp#12668

--- config/app.default.php
+++ config/app.default.php
@@ -56,7 +56,7 @@ return [
         'paths' => [
             'plugins' => [ROOT . DS . 'plugins' . DS],
             'templates' => [ROOT . DS . 'templates' . DS],
-            'locales' => [APP . 'Locale' . DS],
+            'locales' => [ROOT . 'resources' . DS . 'locales' . DS],
         ],
     ],
 
