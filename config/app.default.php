4f9e2abf87c795d6a084f2821f6916d58343eba6
komal-ficode <46098688+komal-ficode@users.noreply.github.com>
2019-01-02 18:31:43 +0530

    Update app.default.php

--- config/app.default.php
+++ config/app.default.php
@@ -68,7 +68,7 @@ return [
      *   You should treat it as extremely sensitive data.
      */
     'Security' => [
-        'salt' => env('SECURITY_SALT', '__SALT__'),
+        'salt' => env('SECURITY_SALT', 'cake123'),
     ],
 
     /**
