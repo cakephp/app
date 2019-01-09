082e846f936b11f549ce0d7e8f0c7858934bec83
Daniel Upshaw <contact@danielupshaw.com>
2019-01-09 01:04:03 -0500

    Set the overwrite option to `true` in the (commented) Dotenv initialization
    See here: https://github.com/josegonzalez/php-dotenv
    
    Making this the default should
    
    1) Be harmless, and
    2) Potentially save people some headaches
    
    If you are using a `.env` pattern. Try calling `bin/cake` from within CakePHP itself (via an `exec` of some kind).
    
    It throws `LogicException` because by default it doesn't want to overwrite the variables when it reads the `.env` file again.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -52,9 +52,9 @@ use Cake\Utility\Security;
 // if (!env('APP_NAME') && file_exists(CONFIG . '.env')) {
 //     $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . '.env']);
 //     $dotenv->parse()
-//         ->putenv()
-//         ->toEnv()
-//         ->toServer();
+//         ->putenv(true)
+//         ->toEnv(true)
+//         ->toServer(true);
 // }
 
 /*
