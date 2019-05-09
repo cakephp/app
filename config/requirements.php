d2548499143086b10d6a55c915abf4fb9af8d3b5
mscherer <mark.scherer@spryker.com>
2019-05-10 01:29:39 +0200

    Fix CS.

--- config/requirements.php
+++ config/requirements.php
@@ -21,19 +21,19 @@
  * You can remove this if you are confident that your PHP version is sufficient.
  */
 if (version_compare(PHP_VERSION, '7.2.0') < 0) {
-    trigger_error('Your PHP version must be equal or higher than 7.2.0 to use CakePHP.' . PHP_EOL, E_USER_ERROR);
+    trigger_error('Your PHP version must be equal or higher than 7.2.0 to use CakePHP.', E_USER_ERROR);
 }
 
 /*
  * You can remove this if you are confident you have intl installed.
  */
 if (!extension_loaded('intl')) {
-    trigger_error('You must enable the intl extension to use CakePHP.' . PHP_EOL, E_USER_ERROR);
+    trigger_error('You must enable the intl extension to use CakePHP.', E_USER_ERROR);
 }
 
 /*
  * You can remove this if you are confident you have mbstring installed.
  */
 if (!extension_loaded('mbstring')) {
-    trigger_error('You must enable the mbstring extension to use CakePHP.' . PHP_EOL, E_USER_ERROR);
+    trigger_error('You must enable the mbstring extension to use CakePHP.', E_USER_ERROR);
 }
