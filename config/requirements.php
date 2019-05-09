969e5ea3c7f676d4026a0dcde832650a7d90d930
saeideng <saeideng@users.noreply.github.com>
2019-05-10 01:52:45 +0430

    "config/requirements.php" Update to PHP 7.2
    Update to PHP 7.2

--- config/requirements.php
+++ config/requirements.php
@@ -20,8 +20,8 @@
 /*
  * You can remove this if you are confident that your PHP version is sufficient.
  */
-if (version_compare(PHP_VERSION, '5.6.0') < 0) {
-    trigger_error('Your PHP version must be equal or higher than 5.6.0 to use CakePHP.' . PHP_EOL, E_USER_ERROR);
+if (version_compare(PHP_VERSION, '7.2.0') < 0) {
+    trigger_error('Your PHP version must be equal or higher than 7.2.0 to use CakePHP.' . PHP_EOL, E_USER_ERROR);
 }
 
 /*
