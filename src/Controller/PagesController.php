824d7a72068058e68ebdec663f1b99edcf8d32b7
mscherer <mark.scherer@spryker.com>
2019-09-07 00:29:55 +0200

    Merge master into 4.x

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -40,12 +40,11 @@ class PagesController extends AppController
      * @throws \Cake\View\Exception\MissingTemplateException When the view file could not
      *   be found and in debug mode.
      * @throws \Cake\Http\Exception\NotFoundException When the view file could not
-     *   be found and not in debug mode.
+     *   be found
      */
     public function display(...$path): ?Response
     {
-        $count = count($path);
-        if (!$count) {
+        if (!$path) {
             return $this->redirect('/');
         }
         if (in_array('..', $path, true) || in_array('.', $path, true)) {
