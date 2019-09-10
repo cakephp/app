61b0cdaa765c373390a76f7a9295f5e5fba818b9
inoas <mail@inoas.com>
2019-09-10 12:37:11 +0200

    auto-load-app-local-and-env

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -44,21 +44,20 @@ use Cake\Utility\Inflector;
 use Cake\Utility\Security;
 
 /**
- * Uncomment block of code below if you want to use `.env` file during development.
- * You should copy `config/.env.default to `config/.env` and set/modify the
- * variables as required.
- *
  * It is HIGHLY discouraged to use a .env file in production, due to security risks
  * and decreased performance on each request. The purpose of the .env file is to emulate
  * the presence of the environment variables like they would be present in production.
+ *
+ * You may copy `config/.env.default to `config/.env` and set/modify the
+ * variables as required.
  */
-// if (!env('APP_NAME') && file_exists(CONFIG . '.env')) {
-//     $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . '.env']);
-//     $dotenv->parse()
-//         ->putenv()
-//         ->toEnv()
-//         ->toServer();
-// }
+if (!env('APP_NAME') && file_exists(CONFIG . '.env')) {
+    $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . '.env']);
+    $dotenv->parse()
+        ->putenv()
+        ->toEnv()
+        ->toServer();
+}
 
 /*
  * Read configuration file and inject configuration into various
@@ -80,7 +79,9 @@ try {
  * You can use a file like app_local.php to provide local overrides to your
  * shared configuration.
  */
-//Configure::load('app_local', 'default');
+if (file_exists(CONFIG . 'app_local.php')) {
+    Configure::load('app_local', 'default');
+}
 
 /*
  * When debug = true the metadata cache should only last
