47fc76dfd0a4cf7bd6f8c3be524c49e54b82c0a8
inoas <mail@inoas.com>
2019-09-25 14:26:16 +0200

    Git

--- config/app_local.example.php
+++ config/app_local.example.php
@@ -3,7 +3,7 @@
  * Local configuration file to provide any overrides to your app.php configuration.
  * Copy and save this file as app_local.php and make changes as required.
  * Note: It is not recommended to commit files with credentials such as app_local.php
- * into source code version control such as git.
+ * into source code version control such as Git.
  */
 return [
     /*
