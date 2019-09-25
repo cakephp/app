89265b48e94fecd3ce9f9fc698f82a4697a57a75
inoas <mail@inoas.com>
2019-09-25 14:24:56 +0200

    rename app_local.php

--- config/app_local.example.php
+++ config/app_local.example.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Local configuration file to provide any overrides to your app.php configuration.
- * Copy as app_local.php and make changes as required.
+ * Copy and save this file as app_local.php and make changes as required.
  * Note: It is not recommended to commit files with credentials such as app_local.php
  * into source code version control such as git.
  */
