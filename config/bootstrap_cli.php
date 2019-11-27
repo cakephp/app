81e14fc5a0d976e9fbcaa4785ae195c8da34659c
ADmad <admad.coder@gmail.com>
2019-11-27 13:11:36 +0530

    Fix CS error

--- config/bootstrap_cli.php
+++ config/bootstrap_cli.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -12,6 +14,7 @@
  * @since         3.0.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
+
 use Cake\Core\Configure;
 
 /*
