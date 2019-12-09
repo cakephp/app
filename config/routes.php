53c253616160ef7c79ed5e92b6b6ddbb42311a3b
mscherer <mark.scherer@spryker.com>
2019-12-10 00:27:38 +0100

    Backport composer commands.

--- config/routes.php
+++ config/routes.php
@@ -48,7 +48,7 @@ Router::defaultRouteClass(DashedRoute::class);
 Router::scope('/', function (RouteBuilder $routes) {
     // Register scoped middleware for in scopes.
     $routes->registerMiddleware('csrf', new CsrfProtectionMiddleware([
-        'httpOnly' => true
+        'httpOnly' => true,
     ]));
 
     /*
