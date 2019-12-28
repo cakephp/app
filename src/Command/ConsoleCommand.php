e67d34b1b3ffef7cfa1cf4346007d171c741fa56
ADmad <admad.coder@gmail.com>
2019-12-28 10:47:19 +0530

    Update to phpstan v0.12

--- src/Command/ConsoleCommand.php
+++ src/Command/ConsoleCommand.php
@@ -33,7 +33,7 @@ class ConsoleCommand extends Command
      *
      * @param \Cake\Console\Arguments $args The command arguments.
      * @param \Cake\Console\ConsoleIo $io The console io
-     * @return int|null The exit code or null for success
+     * @return int|null|void The exit code or null for success
      */
     public function execute(Arguments $args, ConsoleIo $io)
     {
