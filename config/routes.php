072f74215509c29d5341568923f1f40cebc3901f
ADmad <admad.coder@gmail.com>
2020-06-12 01:27:59 +0530

    Move CsrfProtectionMiddleware from routes.php to src/Application.php.
    News users struggle to find out from where the middleware is loaded
    if they want to remove/customize it. Having in inside Application
    class provides better visibility.

--- config/routes.php
+++ config/routes.php
@@ -46,17 +46,6 @@ use Cake\Routing\RouteBuilder;
 $routes->setRouteClass(DashedRoute::class);
 
 $routes->scope('/', function (RouteBuilder $builder) {
-    // Register scoped middleware for in scopes.
-    $builder->registerMiddleware('csrf', new CsrfProtectionMiddleware([
-        'httpOnly' => true,
-    ]));
-
-    /*
-     * Apply a middleware to the current route scope.
-     * Requires middleware to be registered through `Application::routes()` with `registerMiddleware()`
-     */
-    $builder->applyMiddleware('csrf');
-
     /*
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
      * its action called 'display', and we pass a param to select the view file
