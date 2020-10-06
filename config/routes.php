133a2d4c54af408236cf3ab6e0f7d89ec9ba57da
ADmad <ADmad@users.noreply.github.com>
2020-10-06 23:16:32 +0530

    Add example for setting extensions

--- config/routes.php
+++ config/routes.php
@@ -44,7 +44,7 @@ use Cake\Routing\RouteBuilder;
 /** @var \Cake\Routing\RouteBuilder $routes */
 $routes->setRouteClass(DashedRoute::class);
 
-$routes->scope('/', function (RouteBuilder $builder) {
+$routes->scope('/', function (RouteBuilder $builder) {   
     /*
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
      * its action called 'display', and we pass a param to select the view file
@@ -80,6 +80,10 @@ $routes->scope('/', function (RouteBuilder $builder) {
  * ```
  * $routes->scope('/api', function (RouteBuilder $builder) {
  *     // No $builder->applyMiddleware() here.
+ *     
+ *     // Parse specified extensions from URLs
+ *     // $builder->setExtensions(['json', 'xml']);
+ *     
  *     // Connect API actions here.
  * });
  * ```
