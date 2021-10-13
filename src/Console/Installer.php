23a6b1f2a3a337f46e24e59082ef453b0afe9c62
Viktor Szépe <viktor@szepe.net>
2021-10-13 04:17:22 +0200

    Fix name conflict in Installer

--- src/Console/Installer.php
+++ src/Console/Installer.php
@@ -20,7 +20,7 @@ if (!defined('STDIN')) {
     define('STDIN', fopen('php://stdin', 'r'));
 }
 
-use Cake\Codeception\Console\Installer;
+use Cake\Codeception\Console\Installer as CodeceptionInstaller;
 use Cake\Utility\Security;
 use Composer\Script\Event;
 use Exception;
@@ -64,8 +64,8 @@ class Installer
         static::setFolderPermissions($rootDir, $io);
         static::setSecuritySalt($rootDir, $io);
 
-        if (class_exists(Installer::class)) {
-            Installer::customizeCodeceptionBinary($event);
+        if (class_exists(CodeceptionInstaller::class)) {
+            CodeceptionInstaller::customizeCodeceptionBinary($event);
         }
     }
 
