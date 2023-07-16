905b21d35a517806ad9a5ba3331353838dba82a0
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-07-16 14:35:36 +0200

    remove static method in routes.php

--- config/routes.php
+++ config/routes.php
@@ -21,71 +21,73 @@
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 
+/**
+ * @var \Cake\Routing\RouteBuilder $routes
+ */
+
 use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 
-return static function (RouteBuilder $routes) {
+/*
+ * The default class to use for all routes
+ *
+ * The following route classes are supplied with CakePHP and are appropriate
+ * to set as the default:
+ *
+ * - Route
+ * - InflectedRoute
+ * - DashedRoute
+ *
+ * If no call is made to `Router::defaultRouteClass()`, the class used is
+ * `Route` (`Cake\Routing\Route\Route`)
+ *
+ * Note that `Route` does not do any inflections on URLs which will result in
+ * inconsistently cased URLs when used with `{plugin}`, `{controller}` and
+ * `{action}` markers.
+ */
+$routes->setRouteClass(DashedRoute::class);
+
+$routes->scope('/', function (RouteBuilder $builder) {
     /*
-     * The default class to use for all routes
-     *
-     * The following route classes are supplied with CakePHP and are appropriate
-     * to set as the default:
-     *
-     * - Route
-     * - InflectedRoute
-     * - DashedRoute
-     *
-     * If no call is made to `Router::defaultRouteClass()`, the class used is
-     * `Route` (`Cake\Routing\Route\Route`)
-     *
-     * Note that `Route` does not do any inflections on URLs which will result in
-     * inconsistently cased URLs when used with `{plugin}`, `{controller}` and
-     * `{action}` markers.
+     * Here, we are connecting '/' (base path) to a controller called 'Pages',
+     * its action called 'display', and we pass a param to select the view file
+     * to use (in this case, templates/Pages/home.php)...
      */
-    $routes->setRouteClass(DashedRoute::class);
-
-    $routes->scope('/', function (RouteBuilder $builder) {
-        /*
-         * Here, we are connecting '/' (base path) to a controller called 'Pages',
-         * its action called 'display', and we pass a param to select the view file
-         * to use (in this case, templates/Pages/home.php)...
-         */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-
-        /*
-         * ...and connect the rest of 'Pages' controller's URLs.
-         */
-        $builder->connect('/pages/*', 'Pages::display');
-
-        /*
-         * Connect catchall routes for all controllers.
-         *
-         * The `fallbacks` method is a shortcut for
-         *
-         * ```
-         * $builder->connect('/{controller}', ['action' => 'index']);
-         * $builder->connect('/{controller}/{action}/*', []);
-         * ```
-         *
-         * You can remove these routes once you've connected the
-         * routes you want in your application.
-         */
-        $builder->fallbacks();
-    });
+    $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
     /*
-     * If you need a different set of middleware or none at all,
-     * open new scope and define routes there.
-     *
-     * ```
-     * $routes->scope('/api', function (RouteBuilder $builder) {
-     *     // No $builder->applyMiddleware() here.
-     *
-     *     // Parse specified extensions from URLs
-     *     // $builder->setExtensions(['json', 'xml']);
-     *
-     *     // Connect API actions here.
-     * });
-     * ```
+     * ...and connect the rest of 'Pages' controller's URLs.
      */
-};
+    $builder->connect('/pages/*', 'Pages::display');
+
+    /*
+     * Connect catchall routes for all controllers.
+     *
+     * The `fallbacks` method is a shortcut for
+     *
+     * ```
+     * $builder->connect('/{controller}', ['action' => 'index']);
+     * $builder->connect('/{controller}/{action}/*', []);
+     * ```
+     *
+     * You can remove these routes once you've connected the
+     * routes you want in your application.
+     */
+    $builder->fallbacks();
+});
+
+/*
+ * If you need a different set of middleware or none at all,
+ * open new scope and define routes there.
+ *
+ * ```
+ * $routes->scope('/api', function (RouteBuilder $builder) {
+ *     // No $builder->applyMiddleware() here.
+ *
+ *     // Parse specified extensions from URLs
+ *     // $builder->setExtensions(['json', 'xml']);
+ *
+ *     // Connect API actions here.
+ * });
+ * ```
+ */
