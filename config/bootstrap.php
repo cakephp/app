ce7c51317d8b999d0ef9e84c89421f531d9df772
ADmad <ADmad@users.noreply.github.com>
2023-10-21 19:54:06 +0530

    Fix class names in e.g. code.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -227,5 +227,5 @@ ServerRequest::addDetector('tablet', function ($request) {
 // set a custom date and time format
 // see https://book.cakephp.org/5/en/core-libraries/time.html#setting-the-default-locale-and-format-string
 // and https://unicode-org.github.io/icu/userguide/format_parse/datetime/#datetime-format-syntax
-//\Cake\I18n\FrozenDate::setToStringFormat('dd.MM.yyyy');
-//\Cake\I18n\FrozenTime::setToStringFormat('dd.MM.yyyy HH:mm');
+//\Cake\I18n\Date::setToStringFormat('dd.MM.yyyy');
+//\Cake\I18n\Time::setToStringFormat('dd.MM.yyyy HH:mm');
