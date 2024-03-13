46d6b1355debe397d845a196f9253fd5cfca07bf
MarwanSalim <45453458+MarwanSalim@users.noreply.github.com>
2024-03-13 11:06:19 +0800

    Update bootstrap.php
    Remove the setting for the `_cake_routes_` cache since it has been removed in cakePHP 5

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -103,8 +103,6 @@ if (file_exists(CONFIG . 'app_local.php')) {
 if (Configure::read('debug')) {
     Configure::write('Cache._cake_model_.duration', '+2 minutes');
     Configure::write('Cache._cake_core_.duration', '+2 minutes');
-    // disable router cache during development
-    Configure::write('Cache._cake_routes_.duration', '+2 seconds');
 }
 
 /*
