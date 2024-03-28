44d10371c751b974b4d1c4c4ff46eda59c486fda
Umer Salman <umer936@hotmail.com>
2024-03-28 15:17:02 -0500

    Fix missing return type check in ErrorController.php
    Fixes: 
    ( ! ) Fatal error: Declaration of App\Controller\ErrorController::beforeFilter(Cake\Event\EventInterface $event) must be compatible with App\Controller\AppController::beforeFilter(Cake\Event\EventInterface $event): void in /web/hpca/src/Controller/ErrorController.php on line 44

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -41,7 +41,7 @@ class ErrorController extends AppController
      * @param \Cake\Event\EventInterface<\Cake\Controller\Controller> $event Event.
      * @return \Cake\Http\Response|null|void
      */
-    public function beforeFilter(EventInterface $event)
+    public function beforeFilter(EventInterface $event): void
     {
     }
 
