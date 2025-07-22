027dbfc1833d650ca14ff0ee7a7ec3839d361508
Mark Scherer <dereuromark@users.noreply.github.com>
2025-07-22 12:14:41 +0200

    Update AppView.php
    `@link` should be used for linking things together.
    Here, a simple `@see` is correct IMO.
    
    Refs https://github.com/dereuromark/cakephp-ide-helper/blob/b6237cc03a50735bd43a5290e0a9e77972864208/config/app.example.php#L34

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -22,7 +22,7 @@ use Cake\View\View;
  *
  * Your application's default view class
  *
- * @link https://book.cakephp.org/5/en/views.html#the-app-view
+ * @see https://book.cakephp.org/5/en/views.html#the-app-view
  */
 class AppView extends View
 {
